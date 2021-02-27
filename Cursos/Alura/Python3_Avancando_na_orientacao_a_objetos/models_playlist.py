8c380b3f5cf5a8ab9363d8c11b34801688a7161b
ramonvaleriano <rrvaleriano@gmail.com>
2021-02-27 08:06:08 -0300

    Quando não usar a herança

---
+++ Cursos/Alura/Python3_Avancando_na_orientacao_a_objetos/models_playlist.py
@@ -0,0 +1,60 @@
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    def __str__(self):
+        return f'{self.nome} - {self.ano} - {self.likes}'
+
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+    def __str__(self):
+        return f'{self.nome} - {self.ano} - {self.duracao} min - {self.likes}'
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super(Serie, self).__init__(nome, ano)
+        self.temporadas = temporadas
+
+    def __str__(self):
+        return f'{self.nome} - {self.ano} - {self.temporadas} temporadas - {self.likes}'
+
+class Playlist(list):
+    def __init__(self, nome, programas):
+        self.nome = nome.title()
+        super().__init__(programas)
+
+
+
+vingadores = Filme('Vigadores - Guerra Infinita', 2018, 160)
+atlanta = Serie('Atlatan', 2018, 2)
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
+
+print(f'Tamonho do playlist: {len(playlist_fim_de_semana)}')
+
+for dados in playlist_fim_de_semana:
+    print(dados)
