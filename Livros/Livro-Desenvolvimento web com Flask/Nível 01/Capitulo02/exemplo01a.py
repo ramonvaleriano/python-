245510cc9b9e40cab358d874468de4144d647c6f
ramonvaleriano <rrvaleriano@gmail.com>
2021-03-02 09:56:39 -0300

    Dando inicio ao livro de Flask
    Dando inicio a um estudo mais aprofundado em Flask, no desenvolvimento web com python, usando Flask.

---
+++ Livros/Livro-Desenvolvimento web com Flask/Nível 01/Capitulo02/exemplo01a.py
@@ -0,0 +1,9 @@
+# Program: exemplo01a.py
+# Author: Ramon R. Valeriano
+# Description: Primeiro exemplo de uma aplicação web
+# Developed: 02/03/2020 - 09:53
+
+from flask import Flask
+
+app = Flask(__name__)
+
