b6ddcf19356ee68e34ea06e29c5d430ccaa768ae
ramonvaleriano <rrvaleriano@gmail.com>
2021-03-09 10:45:26 -0300

    Resolvendo problemas

--- Livros/Livro-Desenvolvimento web com Flask/Capitulo04/exemplo01a.py
+++ Livros/Livro-Desenvolvimento web com Flask/Capitulo04/exemplo01a.py
@@ -6,11 +6,27 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html', )
+    return render_template('index.html', current_time=datetime.utcnow())
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('userInicial.html', name=name)
+
+@app.errorhandler(404)
+def pagina_nao_encontrada(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def erro_servidor(e):
+    return render_template('500.html'), 500
+
+app.run(debug=True)
