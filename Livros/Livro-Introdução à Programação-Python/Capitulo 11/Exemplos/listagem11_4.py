2234e239b7396fa5d0383b301a665691e3b67831
ramonvaleriano <rrvaleriano@gmail.com>
2021-03-15 14:24:35 -0300

    Consulta de Multiplos resultados.

--- Livros/Livro-Introdução à Programação-Python/Capitulo 11/Exemplos/listagem11_4.py
+++ Livros/Livro-Introdução à Programação-Python/Capitulo 11/Exemplos/listagem11_4.py
@@ -1,4 +1,15 @@
-# Program: Listagem11_3testando.py
+# Program: Listagem11_4.py
 # Author: Ramon R. Valeriano
-# Description: Exemplo de Uso do SQlite, em python
-# Developed: 15/03/2020 - 14:15
+# Description: Consulta de multiplos resultados.
+# Developed: 15/03/2020 - 14:20
+
+import sqlite3
+
+conexao = sqlite3.connect('agenda.db')
+cursos = conexao.cursor()
+cursos.execute('select * from agenda')
+resultado = cursos.fetchall()
+for registro in resultado:
+    print(f'Nome: {registro[0]}, Telefone: {registro[1]}')
+cursos.close()
+conexao.close()
