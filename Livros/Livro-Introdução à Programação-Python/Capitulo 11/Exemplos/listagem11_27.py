273ef34e1594001914b6ca17919f39f15f3ebe6f
ramonvaleriano <rrvaleriano@gmail.com>
2021-03-17 10:03:29 -0300

    Trabalhando com datas

--- Livros/Livro-Introdução à Programação-Python/Capitulo 11/Exemplos/listagem11_27.py
+++ Livros/Livro-Introdução à Programação-Python/Capitulo 11/Exemplos/listagem11_27.py
@@ -4,7 +4,6 @@
 # Developed: 17/03/2020 - 09:42
 
 import sqlite3
-from datetime import datetime
 
 with sqlite3.connect('brasil.db', detect_types=sqlite3.PARSE_DECLTYPES) as conexao:
     conexao.row_factory = sqlite3.Row
@@ -13,3 +12,4 @@ with sqlite3.connect('brasil.db', detect_types=sqlite3.PARSE_DECLTYPES) as conex
         print(f'{feriado["data"].strftime("%d/%m")} {feriado["descricao"]}')
 
     cursor.close()
+
